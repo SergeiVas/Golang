53e234cfeab1d0824999a8a09d6f6236c0576322
SergeiVas <shot.kolt@mail.ru>
2016-12-26 22:47:32 +0700

    Add first comments

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 )
 
+//Переходим по url'у и считаем кол - во искомых строк
 func countStrings(search string, c chan string, count *int, countUrl *int, goryt *int) int {
 	*goryt++
 	url := <-c
@@ -22,8 +23,8 @@ func countStrings(search string, c chan string, count *int, countUrl *int, goryt
 	return strings.Count(string(s), string(search))
 }
 
+// Разделяем url'ы и записываем их в канал
 func stringsUrl(allUrl string, c chan string, countUrl *int) {
-
 	s := ""
 	for i := 0; i < len(allUrl); i++ {
 		if allUrl[i] != 92 {
@@ -38,28 +39,29 @@ func stringsUrl(allUrl string, c chan string, countUrl *int) {
 	c <- string(s)
 }
 
+// Считываем url'ы
 func readFile() string {
 	bs, _ := ioutil.ReadFile("input.txt")
 	str := string(bs)
-
 	formatStr := ""
+	//Начинаем читать с '  и заканчиваем ' чтобы получить только строки url'ов
 	for i := strings.Index(str, "'") + 1; i < len(str)-1; i++ {
 		formatStr += string(str[i])
 	}
-	return formatStr
+	return formatStr // Возвращаем строку из url'ов
 }
 
 func main() {
 	var (
 		search    = "Go"
 		count     = 0
-		countUrl  = 1
+		countUrl  = 1 // Кол - во url'ов, которые необходимо будет обработать
 		gorytines = 0
 	)
-	ch := make(chan string)
-	s := readFile()
+	ch := make(chan string) // Канал, в котором хранятся url
+	s := readFile()         // Записываем в строку необработанные url'ы
 
-	go stringsUrl(string(s), ch, &countUrl)
+	go stringsUrl(string(s), ch, &countUrl) // Обрабатываем url'ы и записываем их количество
 
 	for countUrl > 0 {
 		for i := gorytines; i < 5; i++ {
