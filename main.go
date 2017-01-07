1be9c6254fe740f925c35ea59ec795a29d9df4f3
SergeiVas <shot.kolt@mail.ru>
2017-01-07 17:05:49 +0700

    Second commit

--- main.go
+++ main.go
@@ -2,74 +2,92 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"strings"
+	"sync"
 )
 
+func main() {
+
+	var (
+		count     = 0
+		gorytines = 0
+		k         = 5
+		wg        sync.WaitGroup
+	)
+	ch := make(chan string) // Канал, в котором хранятся url
+	go readFile(ch)
+	for num := range ch {
+		if gorytines < k {
+			wg.Add(1)
+			go countStrings(num, &count, &gorytines, &wg)
+		} else { // Если кол - во горутин больше заданного ждем пока они выполнятся и запускаем снова
+			wg.Wait()
+			wg.Add(1)
+			go countStrings(num, &count, &gorytines, &wg)
+		}
+	}
+	wg.Wait() // Ждем выполнения всех горутин
+
+	fmt.Print("Total: ")
+	fmt.Println(count)
+}
+
 //Переходим по url'у и считаем кол - во искомых строк
-func countStrings(search string, c chan string, count *int, countUrl *int, goryt *int) int {
+func countStrings(url string, count *int, goryt *int, wg *sync.WaitGroup) {
 	*goryt++
-	url := <-c
 	response, _ := http.Get(url)
 	defer response.Body.Close()
 	s, _ := ioutil.ReadAll(response.Body)
 	fmt.Print("Count for " + string(url) + ": ")
-	fmt.Println(strings.Count(string(s), string(search)))
-	*count += strings.Count(string(s), string(search))
-	*countUrl--
+	fmt.Println(strings.Count(string(s), "Go"))
+	*count += strings.Count(string(s), "Go")
 	*goryt--
-	return strings.Count(string(s), string(search))
+	wg.Done()
 }
 
-// Разделяем url'ы и записываем их в канал
-func stringsUrl(allUrl string, c chan string, countUrl *int) {
+func initializationUrl(line string, num int, c chan string) {
 	s := ""
-	for i := 0; i < len(allUrl); i++ {
-		if allUrl[i] != 92 {
-			s += string(allUrl[i])
+	// Считываем посимвольно строку пока не встретим "/n" и отправляем в канал
+	for i := num; i < len(line); i++ {
+		if line[i] != 92 {
+			s += string(line[i])
 		} else {
 			c <- string(s)
 			s = ""
 			i++
-			*countUrl++
 		}
 	}
-	c <- string(s)
-}
-
-// Считываем url'ы
-func readFile() string {
-	bs, _ := ioutil.ReadFile("input.txt")
-	str := string(bs)
-	formatStr := ""
-	//Начинаем читать с '  и заканчиваем ' чтобы получить только строки url'ов
-	for i := strings.Index(str, "'") + 1; i < len(str)-1; i++ {
-		formatStr += string(str[i])
+	// В конце "/n" не встречается, поэтому просто отправляем url в канал
+	if len(s) > 5 {
+		c <- string(s)
+		close(c)
 	}
-	return formatStr // Возвращаем строку из url'ов
 }
 
-func main() {
-	var (
-		search    = "Go"
-		count     = 0
-		countUrl  = 1 // Кол - во url'ов, которые необходимо будет обработать
-		gorytines = 0
-	)
-	ch := make(chan string) // Канал, в котором хранятся url
-	s := readFile()         // Записываем в строку необработанные url'ы
+func readFile(c chan string) {
+	file, _ := os.Open("input.txt")
+	f := bufio.NewReader(file)
+	check := false
+	// Считываем строки с файла и отправляем их на разбор url'ов
+	for {
+		line, err := f.ReadString('\n')
+		line = strings.Replace(line, "\r\n'", "", -1)
+		line = strings.Replace(line, "'", "", -1)
+		if check == false {
+			initializationUrl(line, 10, c) // если первая строка, то сразу игнорируем "$ echo -e "
+			check = true
+		} else {
+			initializationUrl(line, 0, c)
 
-	go stringsUrl(string(s), ch, &countUrl) // Обрабатываем url'ы и записываем их количество
-
-	for countUrl > 0 {
-		for i := gorytines; i < 5; i++ {
-			go countStrings(search, ch, &count, &countUrl, &gorytines)
 		}
+		if err != nil {
+			break
+		}
+
 	}
-
-	fmt.Print("Total: ")
-	fmt.Println(count)
-
 }
